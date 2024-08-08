f7cd691dd751b0eebbd1adb6160fbc5b97b99db8
ccslon <143284758+ccslon@users.noreply.github.com>
2024-08-08 17:47:44 -0400

    Assembler sorta working

---
+++ assembler.py
@@ -0,0 +1,404 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 25 10:49:03 2023
+
+@author: ccslon
+"""
+
+import re
+from bit32 import Size, Reg, FReg, Op, Cond, Jump, Binary, Char, unescape
+
+RE_SIZE = r'|'.join(size.name for size in Size)
+RE_OP = r'|'.join(op.name for op in Op)
+RE_COND = r'|'.join(cond.name for cond in Cond)
+
+TOKENS = {
+    'const': r'-?(0x[0-9a-f]+|0b[01]+|\d+)',
+    'string': r'"[^"]*"',
+    'char': r"'\\?[^']'",
+    'ldm': r'^(ldm)\b',
+    'ld': r'^(ld)\b',
+    'nop': r'^(nop)\b',
+    'push': r'^(push)\b',
+    'pop': r'^(pop)\b',
+    'call': r'^(call)\b',
+    'ret': r'^(ret)\b',
+    'halt': r'^(halt)\b',
+    'space': r'\b(space)\b',
+    'reg': r'\b('+r'|'.join(reg.name for reg in Reg)+'|'+'|'.join(reg.name for reg in FReg)+r')\b',
+    'op': rf'^(?P<name>{RE_OP})(?P<cond>{RE_COND})?(?P<flags>s)?(\.(?P<size>{RE_SIZE}))?\b',
+    'jump': rf'^j(mp)?(?P<jcond>{RE_COND})?\b',
+    'label': r'\.?[a-z_]\w*\s*:',
+    'id': r'\.?[a-z_]\w*',
+    'equal': r'=',
+    'lbrace': r'\[',
+    'rbrace': r'\]',
+    'lbrack': r'\{',
+    'rbrack': r'\}',
+    'comma': r',',
+    'end': r'$',
+    'error': r'\S+'
+}
+
+RE = re.compile('|'.join(rf'(?P<{token}>{pattern})' for token, pattern in TOKENS.items()), re.I)
+
+def lex(text):
+    return [(match.lastgroup, match.group(), match) for match in RE.finditer(text)]
+
+MYRE = re.compile('(?P<op>add)|(?P<flag>(?<=add)s?)', re.I)
+
+def mylex(text):
+    return [(match.lastgroup, match.group(), match) for match in MYRE.finditer(text)]
+
+class Assembler:
+    def label(self, label):
+        self.labels.append(label)
+    def const(self, label, value):
+        self.label(label)
+        self.new_data(value)
+    def char(self, label, char):
+        self.label(label)
+        self.new_char(char)
+    def string(self, label, string):
+        self.label(label)
+        for char in string:
+            self.new_char(char)
+    def space(self, label, size):
+        self.label(label)
+        for _ in range(size):
+            self.new_data(0)
+    def jump(self, cond, label):
+        self.new_inst(Jump, cond, label)
+    def binary(self, op, cond, flag, size, rd, rs):
+        self.new_inst(Binary, cond, flag, size, False, op, rd, rs)
+    def binary_const(self, op, cond, flag, size, rd, rs):
+        self.new_inst(Binary, cond, flag, size, True, op, rd, rs)
+    # def load_reg(self, rd, rb, ro):
+    #     self.new_inst(LoadReg, False, rd, rb, ro)
+    # def load(self, rd, rb, offset):
+    #     if rb == Reg.FP:
+    #         self.new_inst(LoadFP, False, rd, offset)
+    #     else:
+    #         self.new_inst(Load, False, rd, rb, offset)
+    # def store0(self, rb, rd):
+    #     self.new_inst(Load, True, rd, rb, 0)
+    # def store_reg(self, rb, ro, rd):
+    #     self.new_inst(LoadReg, True, rd, rb, ro)
+    # def store(self, rb, offset, rd):
+    #     if rb == Reg.FP:
+    #         self.new_inst(LoadFP, True, rd, offset)
+    #     else:
+    #         self.new_inst(Load, True, rd, rb, offset)
+    # def pop(self, rd):
+    #     self.new_inst(StackOp, False, rd)
+    # def push(self, rd):
+    #     self.new_inst(StackOp, True, rd)
+    # def load_word(self, rd, value):
+    #     self.new_inst(Word, rd)
+    #     self.new_imm(value)
+    # def op_byte(self, op, rd, byte):
+    #     assert op in [Op.MOV, Op.ADD, Op.SUB, Op.CMP]
+    #     self.new_inst(OpByte, op, byte, rd)
+    # def op1(self, op, rd):
+    #     self.new_inst(Op4, False, op, rd, rd)
+    # def op4(self, op, rd, rs):
+    #     self.new_inst(Op4, False, op, rd, rs)
+    # def op_const(self, op, rd, const):
+    #     if -16 <= const < 16:
+    #         self.new_inst(Op4, True, op, rd, const)
+    #     elif 0 <= const < 256:
+    #         assert op in [Op.MOV, Op.ADD, Op.SUB, Op.CMP]
+    #         self.op_byte(op, rd, const)
+    #     else:
+    #         assert op == Op.MOV
+    #         self.load_word(rd, const)
+    # def offset(self, op, rd, rs, const):
+    #     assert op in [Op.ADD, Op.SUB]
+    #     if rs == Reg.FP:
+    #         self.new_inst(OffsetFP, op, rd, const)
+    #     else:
+    #         self.new_inst(Offset, op, rd, rs, const)
+    def new_inst(self, inst, *args):
+        self.inst.append((self.labels, inst, args))
+        self.labels = []
+    # def new_imm(self, value):
+    #     self.inst.append((self.labels, Data, (value,)))
+    #     self.labels = []
+    # def new_data(self, value):
+    #     self.data.append((self.labels, Data, (value,)))
+    #     self.labels = []
+    # def new_char(self, char):
+    #     self.data.append((self.labels, Char, (char,)))
+    #     self.labels = []
+    # def name(self, name, value):
+    #     self.names[name] = value
+
+    def assemble(self, asm):
+        # with open('boot.s') as bios:
+        #     base = bios.read()
+        base = ''
+        self.inst = []
+        self.data = []
+        self.labels = []
+        self.names = {}
+        for self.line_no, line in enumerate(map(str.strip, (base+'\n'+asm).strip().split('\n'))):
+            if ';' in line:
+                line, comment = map(str.strip, line.split(';', 1))
+            if line:
+                self.tokens = lex(line)
+                self.index = 0
+                print(self.tokens)
+                
+                if self.match('id', '=', 'const'):
+                    print(f'{self.line_no: >2}|{line}')
+                    self.name(*self.values())
+                    
+                elif self.peek('label'):
+                    print(f'{self.line_no: >2}|{line}')
+                    
+                    if self.match('label'):
+                        self.label(*self.values())
+                        
+                #     elif self.match('label', 'const'):
+                #         self.const(*self.values())
+                        
+                #     elif self.match('label', 'id'):
+                #         self.const(*self.values())
+                        
+                #     elif self.match('label', 'char'):
+                #         self.char(*self.values())
+                        
+                #     elif self.match('label', 'string'):
+                #         self.string(*self.values())
+                        
+                #     elif self.match('label', 'space', 'const'):
+                #         self.space(*self.values())
+                        
+                #     else:
+                #         self.error()
+                else:
+                    print(f'{self.line_no: >2}|  {line}')
+                    
+                    if self.match('nop'):
+                        self.jump(Cond.NV, 0)
+                    
+                #     elif self.match('id'):
+                #         self.new_data(*self.values())
+                #     elif self.match('const'):
+                #         self.new_data(*self.values())
+                #     elif self.match('char'):
+                #         self.new_char(*self.values())
+                        
+                    elif self.match('jump', 'id'):
+                        self.jump(*self.values())
+                    
+                    elif self.match('op', 'reg', ',', 'reg'):
+                        self.binary(*self.values())
+                        
+                    elif self.match('op', 'reg', ',', 'const'):
+                        self.binary_const(*self.values())
+                    # elif self.match('op', )
+                        
+                #     elif self.match('op', 'reg'):
+                #         self.op1(*self.values())
+                #     elif self.match('op', 'reg', ',', 'reg'):
+                #         self.op4(*self.values())
+                #     elif self.match('op', 'reg', ',', 'const'):
+                #         self.op_const(*self.values())
+                #     elif self.match('op', 'reg', ',', 'char'):
+                #         self.op_byte(*self.values())
+                #     elif self.match('op', 'reg', ',', 'reg', ',', 'const'):
+                #         self.offset(*self.values())
+                            
+                #     elif self.match('ld', 'reg', ',', '[', 'reg', ']'):
+                #         self.load(*self.values(), 0)
+                #     elif self.match('ld', 'reg', ',', '[', 'reg', ',', 'reg', ']'):
+                #         self.load_reg(*self.values())
+                #     elif self.match('ld', 'reg', ',', '[', 'reg', ',', 'const', ']'):
+                #         self.load(*self.values())
+                                            
+                #     elif self.match('ld', '[', 'reg', ']', ',', 'reg'):
+                #         self.store0(*self.values())
+                #     elif self.match('ld', '[', 'reg', ',', 'reg', ']', ',', 'reg'):
+                #         self.store_w_reg_offset(*self.values())
+                #     elif self.match('ld', '[', 'reg', ',', 'const', ']', ',', 'reg'):
+                #         self.store(*self.values())
+                        
+                #     elif self.match('ld', 'reg', ',', 'const'):
+                #         self.load_word(*self.values())
+                #     elif self.match('ld', 'reg', ',', '=', 'id'):
+                #         self.load_word(*self.values())
+                        
+                #     elif self.accept('push'):
+                #         args = [self.expect('reg')]
+                #         while self.accept(','):
+                #             args.append(self.expect('reg'))
+                #         self.expect('end')
+                #         for reg in args:
+                #             self.push(reg)
+                            
+                #     elif self.accept('pop'):
+                #         args = [self.expect('reg')]
+                #         while self.accept(','):
+                #             args.append(self.expect('reg'))
+                #         self.expect('end')
+                #         for reg in reversed(args):
+                #             self.pop(reg)
+                        
+                #     elif self.match('jump', 'id'):
+                #         self.load_word(Reg.PC, *self.values())
+                        
+                #     elif self.match('call','reg'):
+                #         self.offset(Op.ADD, Reg.LR, Reg.PC, 2)
+                #         self.op4(Op.MOV, Reg.PC, *self.values())
+                        
+                #     elif self.match('call', 'id'):
+                #         self.offset(Op.ADD, Reg.LR, Reg.PC, 3)
+                #         self.load_word(Reg.PC, *self.values())
+                        
+                #     elif self.match('ret'):
+                #         self.op4(Op.MOV, Reg.PC, Reg.LR)
+                    
+                #     elif self.accept('ldm'):
+                #         if self.accept('{'):
+                #             regs = [self.expect('reg')]
+                #             while self.accept(','):
+                #                 regs.append(self.expect('reg'))
+                #             self.expect('}')
+                #             self.expect(',')
+                #             dest = self.expect('reg')
+                #             for i, reg in enumerate(regs):
+                #                 self.load(reg, dest, i)
+                #         else:
+                #             dest = self.expect('reg')
+                #             self.expect(',')
+                #             self.expect('{')
+                #             regs = [self.expect('reg')]
+                #             while self.accept(','):
+                #                 regs.append(self.expect('reg'))
+                #             self.expect('}')
+                #             for i, reg in enumerate(regs):
+                #                 self.store(dest, i, reg)
+                        
+                #     elif self.match('halt'):
+                #         self.op4(Op.MOV, Reg.PC, Reg.PC)
+                    else:
+                        self.error()
+        return self.inst + self.data
+
+    def trans(self, type, value):
+        if type == 'const':
+            if value.startswith('0x'):
+                return int(value, base=16)
+            elif value.startswith('0b'):
+                return int(value, base=2)
+            else:
+                return int(value)
+        elif type in ['string', 'char']:
+            return unescape(value[1:-1])
+        elif type == 'reg':
+            return Reg[value.upper()]
+        elif type == 'label':
+            return value[:-1].strip()
+        elif type == 'id':
+            if value in self.names:
+                return self.names[value]
+            return value
+
+    def values(self):
+        for type, value, match in self.tokens:
+            if type in ['const','string','char','reg','label','id']:
+                yield self.trans(type, value)
+            elif type == 'op':
+                yield Op[match['name'].upper()]
+                yield Cond[match['cond'].upper()] if match['cond'] else Cond.AL
+                yield bool(match['flags'])
+                yield Size[match['size'].upper()] if match['size'] else Size.W
+            elif type == 'jump':
+                yield Cond[match['jcond'].upper()] if match['jcond'] else Cond.AL
+
+    def match(self, *pattern):
+        pattern += ('end',)
+        return len(self.tokens) == len(pattern) and all(self.peek(symbol, offset=i) for i, symbol in enumerate(pattern))
+
+    def __next__(self):
+        type, value, _ = self.tokens[self.index]
+        self.index += 1
+        if type in ['op','jump','const','string','char','reg','cond','label','id']:
+            return self.trans(type, value)
+        return value
+
+    def peek(self, symbol, offset=0):
+        type, value, _ = self.tokens[self.index+offset]
+        return type == symbol or (not value.isalnum() and value == symbol)
+
+    def accept(self, symbol):
+        if self.peek(symbol):
+            return next(self)
+
+    def expect(self, symbol):
+        if self.peek(symbol):
+            return next(self)
+        self.error(expected=symbol)
+
+    def error(self):
+        etype, evalue, _ = self.tokens[self.index]
+        raise SyntaxError(f'Unexpected {etype} token "{evalue}" at token #{self.index} in line {self.line_no}')
+
+class Linker:
+    def link(objects):
+        targets = {}
+        indices = set()
+        print(objects)
+        for i, (labels, type, args) in enumerate(objects):
+            for label in labels:
+                targets[label] = i
+                indices.add(i)
+            objects[i] = (type, args)
+        print('-'*67)
+        print(targets)
+        contents = []
+        for i, (type, args) in enumerate(objects):
+            if args and type is not Char:
+                *args, last = args
+                if isinstance(last, str):
+                    last = targets[last]
+                    if type is Jump:
+                        last = 4 * (last-i)
+                args = *args, last
+            data = type(*args)
+            contents.append(data.little_end())
+            print('>>' if i in indices else '  ', f'{i*4:06x}', f'{data.str: <15}', f'| {data.format_dec(): <13}', f'{data.format_bin(): <22}', data.hex())
+        print('\n', ' '.join(contents))
+        print(len(contents))
+        return contents
+
+# assembler = Assembler()
+
+# def assemble(program, fflag=True, name='out'):
+#     if program.endswith('.s'):
+#         name = program[:-2]
+#         with open(program) as file:
+#             program = file.read()
+#     objects = assembler.assemble(program)
+#     bit16 = Linker.link(objects)
+#     if fflag:
+#         with open(f'{name}.bit16', 'w+') as file:
+#             file.write('v2.0 raw\n' + ' '.join(bit16))
+
+test = '''
+mov A, 10
+mov B, 0
+loop:
+    cmp A, B
+    jge done
+    add B, 1
+    jmp loop
+done:
+    nop
+'''
+
+if __name__ == '__main__':
+    ass = Assembler()
+    objs = ass.assemble(test)
+    Linker.link(objs)
--- bit32.py
+++ bit32.py
@@ -10,8 +10,13 @@ from enum import IntEnum
 def negative(num, base):
     return (-num ^ (2**base - 1)) + 1
 
+class Size(IntEnum):
+    B = 0
+    H = 1
+    W = 2
+
 class Reg(IntEnum):
-    r = 0
+    A = 0
     B = 1
     C = 2
     D = 3
@@ -25,30 +30,167 @@ class Reg(IntEnum):
     PC = 15
     
 class FReg(IntEnum):
-    fA = 8
-    fB = 9
-    fC = 10
-    fD = 11
+    F0 = FA = 8
+    F1 = FB = 9
+    F2 = FC = 10
+    F3 = FD = 11
 
 class Op(IntEnum):
     MOV = 0
-    MVN = 
     ADD = 1
-    SUB = 2
-    CMP = 3
-    MUL = 4
-    NOT = 5
-    DIV = 6
-    MOD = 7
-    AND = 8
-    OR =  9
-    XOR = 10
-    # = 11
-    # = 12
-    NEG = 13
-    SHR = 14
-    SHL = 15
+    ADC = 2
+    CMN = 3
+    MVN = 4
+    SUB = 5
+    SBC = 6
+    CMP = 7
+    NOT = 8
+    NEG = 9
+    AND = 10
+    TST = 11
+    OR = 12
+    # = 13
+    XOR = 14
+    TEQ = 15
+    ITF = 16
+    ADDF = 17
+    SUBF = 18
+    MULF = 19
+    DIVF = 20
+    NEGF = 21
+    #CMPF = 22
+    FTI = 23
+    MUL = 24
+    DIV = 25
+    MOD = 26
+    SHL = 27
+    SHR = 28
+    ASL = 29
+    ROL = 30
+    ROR = 31
+    
+class Cond(IntEnum):
+    NV = 0
+    EQ = 1
+    NE = 2
+    MI = 3
+    PL = 4
+    VS = 5
+    VC = 6
+    CS = HS = 7
+    CC = LO = 8
+    HI = 9
+    LS = 10
+    LT = 11
+    GE = 12
+    LE = 13
+    GT = 14
+    AL = 15
+    
+ESCAPE = {
+    '\0': r'\0',
+    '\t': r'\t',
+    '\n': r'\n',
+    '\b': r'\b',
+    '\\': r'\\'
+}
+UNESCAPE = {
+    r'\n': '\n',
+    r'\0': '\0',
+    r'\t': '\t',
+    r'\b': '\b',
+    r'\\': '\\'
+}
+def unescape(text):
+    for k, v in UNESCAPE.items():
+        text = text.replace(k, v)
+    return text
 
+def escape(char):
+    return ESCAPE.get(char, char)
+
+class Data:
+    def __int__(self):
+        return int(''.join(self.bin).replace('X','0'), base=2)
+    def little_end(self):
+        bin = int(self)
+        return f'{bin&0xff:02x} {(bin&0xff00) >> 8:02x} {(bin&0xff0000) >> 16:02x} {(bin&0xff000000) >> 24:02x}'
+    def hex(self):
+        return f'{int(self):04x}'
+    def format_bin(self):
+        return ' '.join(self.bin)
+    def format_dec(self):
+        return ' '.join(map(str,self.dec)) #map(int,self.dec)))
+
+class Byte(Data):
+    pass
+
+class Half(Data):
+    pass
+
+class Word(Data):
+    pass
+
+class Char(Byte):
+    pass
+
+class Inst(Word):
+    pass
+
+class Jump(Inst):
+    def __init__(self, cond, offset24):
+        #assert -256 <= offset24 < 256
+        if offset24 < 0:
+            self.str = f'J{cond.name} -0x{-offset24:06X}'
+        else:
+            self.str = f'J{cond.name} 0x{offset24:06X}'
+        self.dec = cond,0,0,offset24
+        if offset24 < 0:
+            offset24 = negative(offset24, 24)
+        self.bin = f'{cond:04b}','0','000',f'{offset24:024b}'
+class Binary(Inst):
+    def __init__(self, cond, flags, size, imm, op, rd, src):
+        if imm:
+            self.str = f'{op.name}{cond.name}{"S" if flags else ""}.{size.name} {rd.name}, {src}'
+        else:
+            self.str = f'{op.name}{cond.name}{"S" if flags else ""}.{size.name} {rd.name}, {src.name}'
+        self.dec = cond,int(flags),1,size,int(imm),op,src,0,rd
+        if imm:
+            if src < 0:
+                src = negative(src, 8)
+            self.bin = f'{cond:04b}',f'{flags:b}','001',f'{size:02b}','1',f'{op:05b}',f'{src:08b}',f'{rd:04b}',f'{rd:04b}'
+        else:
+            self.bin = f'{cond:04b}',f'{flags:b}','001',f'{size:02b}','0',f'{op:05b}','XXXX',f'{src:04b}',f'{rd:04b}',f'{rd:04b}'
+        
+
+class Unary(Inst):
+    pass
+class Tertiary(Inst):
+    def __init__(self, cond, flags, size, imm, op, rd, rs, src):
+        if imm:
+            self.str = f'{op.name}{cond.name}{"S" if flags else ""}.{size.name} {rd.name}, {src}'
+        else:
+            self.str = f'{op.name}{cond.name}{"S" if flags else ""}.{size.name} {rd.name}, {src.name}'
+        self.dec = cond,int(flags),1,size,int(imm),op,src,0,rd
+        if imm:
+            if src < 0:
+                src = negative(src, 8)
+            self.bin = f'{cond:04b}',f'{flags:b}','001',f'{size:02b}','1',f'{op:05b}',f'{src:08b}','XXXX',f'{rd:04b}'
+        else:
+            self.bin = f'{cond:04b}',f'{flags:b}','001',f'{size:02b}','0',f'{op:05b}','XXXX',f'{src:04b}','XXXX',f'{rd:04b}'
+
+class Load(Inst):
+    pass
+
+class StackOp(Inst):
+    pass
+
+class Interrupt(Inst):
+    pass
+
+class Immediate(Inst):
+    pass
+    
 '''
 add
 cmn
