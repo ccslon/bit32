f3c926acbd7a27550f12848a6bb61565d4ba7848
ccslon <143284758+ccslon@users.noreply.github.com>
2024-09-28 02:26:43 -0400

    Update sort.c

--- c/sort.c
+++ c/sort.c
@@ -4,17 +4,10 @@
 
 #define LEN 5
 
-int intcmp(int a, int b) {
-    return a - b;
+int intcmp(int* a, int* b) {
+    return *a - *b;
 }
 #define STRS_LEN 4
-char *strs[STRS_LEN] = {
-    "Hello",
-    "goodbye",
-    "cats",
-    "yee",
-    "skill issue"
-};
 int arr[LEN] = {4, 6, 2,3,1};
 
 void main() {
@@ -24,16 +17,8 @@ void main() {
     for (i = 0; i < LEN; i++)
         printf("%d ", arr[i]);
     putchar('\n');
-    qsort(arr, 0, LEN-1, &intcmp);
+    qsort(arr, sizeof(int), 0, LEN-1, intcmp);
     for (i = 0; i < LEN; i++)
         printf("%d ", arr[i]);
-    putchar('\n');
     
-    for (i = 0; i < STRS_LEN; i++) {
-        puts(strs[i]);
-    }
-    qsort(strs, 0, STRS_LEN-1, &strcmp);
-    for (i = 0; i < STRS_LEN; i++) {
-        puts(strs[i]);
-    }
 }
