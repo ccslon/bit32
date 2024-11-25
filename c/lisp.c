2ced210a5415e4e613bfacb26440a1f37e47bff9
ccslon <143284758+ccslon@users.noreply.github.com>
2024-11-25 17:18:33 -0500

    Update lisp.c

--- c/lisp.c
+++ c/lisp.c
@@ -1,16 +1,64 @@
+#define LINE_LENGTH 32
 
-typedef struct _node_ {
-    void* value
-    struct _node_* next;
-} Node;
+typedef unsigned size_t;
+char in();
+void out(char);
 
-Node cons(void* value, Node* next) {
-    Node new = {value, next};
-    return new;
+char buffer[LINE_LENGTH];
+
+size_t strlen(const char* s) {
+    size_t l = 0;
+    while (s[l] != '\0')
+        l++;
+    return l;
 }
 
-/*
-S_EXPR -> ATOM | LIST
-ATOM -> const|id
-LIST -> '(' [S_EXPR {' ' S_EXPR}] ')'
-*/
+int strcmp(const char* s, const char* t, size_t size) {
+    size_t i;
+    for (i = 0; i < size && s[i] == t[i]; i++)
+        if (s[i] == '\0')
+            return 0;
+    return s[i] - t[i];
+}
+
+int get_line(char* s, size_t size) {
+    char c;
+    size_t i = 0;
+    while ((c = in()) != '\n') {
+        if (c) {
+            if (c == '\b') {
+                out(c);
+                if (i > 0)
+                    i--;
+            } else if (i < size-1) {
+                out(c);
+                s[i++] = c;
+            }
+        }
+    }    
+    s[i] = '\0';
+    return i;
+}
+
+void print(char* s) {
+    size_t i;
+    for (i = 0; s[i] != '\0'; i++) {
+        out(s[i]);
+    }
+}
+
+int main() {
+    buffer[0] = '\0';
+    while (1) {
+        get_line(buffer, LINE_LENGTH);
+        if (strcmp(buffer, "quit", LINE_LENGTH) == 0) {
+            print("\nGoodbye\n");
+            break;
+        } else if (buffer[0] == '(') {
+            //eval
+        } else {
+            print("\nUnknown command\n");
+        }
+    }
+    return 0;
+}
