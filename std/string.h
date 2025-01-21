a3d074de0a10954a7892716cd107eae11c73ee4e
ccslon <143284758+ccslon@users.noreply.github.com>
2025-01-20 22:30:13 -0500

    Update string.h

--- std/string.h
+++ std/string.h
@@ -20,23 +20,22 @@ char* strncpy(char* s, const char* t, size_t n) {
 char* strdup(char* s) {
     void* malloc(unsigned);
     char* p = malloc(strlen(s)+1);
-    if (p != (void*)0)
-        strncpy(p, s, strlen(s));
+    // if (p != (void*)0)
+        strncpy(p, s, strlen(s)+1);
     return p;
 }
 char* strcat(char* s, const char* t) {
     size_t i = strlen(s), j = 0;
     while((s[i++] = t[j++]) != '\0')
         ;
+    return s;
 }
 char* strrev(char* s) {
-    size_t front = 0, back = strlen(s) - 1;
-    while (front < back) {
+    size_t front, back;
+    for (front = 0, back = strlen(s)-1; front < back; front++, back--) {
         char temp = s[front];
         s[front] = s[back];
         s[back] = temp;
-        front++;
-        back--;
     }
     return s;
 }
@@ -58,7 +57,7 @@ char* strchr(const char* s, char c) {
     size_t i;
     for (i = 0; s[i] != '\0'; i++)
         if (s[i] == c)
-            return s+i;
+            return &s[i];
     return (char*)0;
 }
 void* memset(void* s, unsigned char v, size_t n) {
