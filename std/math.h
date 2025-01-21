12acc2f7c77023dee677316cc460c2ecd1189c5b
ccslon <143284758+ccslon@users.noreply.github.com>
2025-01-20 22:33:32 -0500

    Playing around with math

--- std/math.h
+++ std/math.h
@@ -1,8 +1,36 @@
-int pow(int base, int exp) {
-    int p = 1;
+#define ITERATIONS 10
+
+unsigned int factorial(unsigned int n) {
+    unsigned int factorial;
+    for (factorial = 1; n > 0; n--) {
+        factorial *= n;
+    }
+    return factorial;
+}
+
+float sum(float x, float (*f)(float, unsigned int)) {
+    float sum = 0;
+    unsigned int n;
+    for (n = 0; n < ITERATIONS; n++) {
+        sum += (*f)(x, n);
+    }
+    return sum;
+}
+
+float pow(float base, unsigned int exp) {
+    float pow = 1;
     while (exp > 0) {
-        p *= base;
+        pow *= base;
         exp--;
     }
-    return p;
+    return pow;
+}
+
+float sin(float t) {
+    float sin = 0;
+    unsigned int n;
+    for (n = 0; n < ITERATIONS; n++) {
+        sin += (pow(-1,n) / factorial(2*n + 1)) * pow(t, 2*n + 1);
+    }
+    return sin;
 }
