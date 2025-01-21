9c853b80e3de488b6856473c27c6d94815e78953
ccslon <143284758+ccslon@users.noreply.github.com>
2025-01-20 22:33:15 -0500

    Adjusted

--- std/stdarg.h
+++ std/stdarg.h
@@ -1,4 +1,4 @@
 typedef int* va_list;
-#define va_start(ap, last) (ap = (int*)&(last)+4)
-#define va_arg(ap, type) ((type)*ap++)
+#define va_start(ap, last) (ap = (va_list)(&last + 1))
+#define va_arg(ap, type) (*(type*)ap++)
 #define va_end(ap) (ap = (void*)0)
