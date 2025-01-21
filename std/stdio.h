c335f55106634cbbd68f8b145bb08438e739e922
ccslon <143284758+ccslon@users.noreply.github.com>
2025-01-20 22:32:34 -0500

    Fixed fprint and added eprint and oprint

--- std/stdio.h
+++ std/stdio.h
@@ -29,7 +29,6 @@ char* fgets(char* s, size_t n, FILE* stream) {
     while (--n > 0 && (c = fgetc(stream))) // "enter"
         if ((*cs++ = c) == '\0')
             break;
-    //*cs = '\0';
     return s;
 }
 char* gets(char* s, size_t n) {
@@ -40,7 +39,7 @@ int fputc(char c, FILE* stream) {
     stream->write = (stream->write + 1) % stream->size;
     return 0;
 }
-#define putc(c) (fputc(c, &stdout))
+#define putc(c) (fputc((c), &stdout))
 int putchar(char c) {
     stdout.buffer[stdout.write] = c;
     stdout.write = (stdout.write + 1) % stdout.size;
@@ -58,10 +57,15 @@ int puts(const char* s) {
     putchar('\n');
     return 0;
 }
-void uprint(unsigned int n) {
+void uprint(unsigned n) {
     if (n / 10)
         uprint(n / 10);
     putchar(n % 10 + '0');
+}
+void oprint(unsigned n) {
+    if (n / 8)
+        oprint(n / 8);
+    putchar(n % 8 + '0');
 }
 void dprint(int n) {
     if (n < 0) {
@@ -70,7 +74,7 @@ void dprint(int n) {
     }
     uprint(n);
 }
-void xprint(unsigned int n, char uplo) {
+void xprint(unsigned n, char uplo) {
     if (n / 16)
         xprint(n / 16, uplo);
     if (n % 16 > 9)
@@ -83,51 +87,90 @@ void fprint(float f, char prec) {
         putchar('-');
         f = -f;
     }
-    int left = f;
-    dprint(left);
-    putchar('.');
-    int p;
-    for (p = 1; prec > 0; --prec) p *= 10;
-    int right = (f - left) * p;
-    dprint(right);
+    unsigned left = f;
+    uprint(left);
+    if (prec > 0) {
+        putchar('.');
+        float right = f - left;
+        do {
+            right *= 10.0;
+            putchar((int)right + '0');
+            right -= (int)right;
+        } while (--prec > 0);
+    }
 }
+void eprint(float f, char prec) {
+    if (f < 0) {
+        putchar('-');
+        f = -f;
+    }
+    int exp = 0;
+    if (f) {
+        while (f >= 10.0) {
+            exp++;
+            f /= 10.0;
+        }
+        while (f < 1.0) {
+            exp--;
+            f *= 10.0;
+        }
+    }    
+    fprint(f, prec);
+    putchar('e');
+    dprint(exp);
+}
+#define va_arg(ap, type) (*(type*)ap++)
 void printf(const char* format, ...) {
     int* ap;
-    (ap = (int*)&(format)+4);
+    (ap = (int*)(&format + 1));
     const char* c;
-    for (c = format; *c; c++) {
+    unsigned n = 0;
+    for (c = format; *c; c++, n++) {
         if (*c == '%') {
-            switch (*++c) {
-                case 'u': {
-                    uprint(((unsigned int)*ap++));
+            c++;
+            char precision = 0; 
+            if ('0' <= *c && *c <= '9') {
+                precision = *c++ - '0';
+            }
+            switch (*c) {
+                case 'u':
+                    uprint(va_arg(ap, unsigned));
                     break;
-                }
-                case 'i': ;
-                case 'd': {
-                    dprint(((int)*ap++));
+                case 'd':
+                case 'i':
+                    dprint(va_arg(ap, int));
                     break;
-                }
-                case 'x': {
-                    xprint(((unsigned int)*ap++), 'a');
+                case 'x':
+                    xprint(va_arg(ap, unsigned), 'a');
                     break;
-                }
-                case 'X': {
-                    xprint(((unsigned int)*ap++), 'A');
+                case 'X':
+                    xprint(va_arg(ap, unsigned), 'A');
                     break;
-                }
-                case 's': {
-                    printf(((char*)*ap++));
+                case 'f':
+                    fprint(va_arg(ap, float), precision);
                     break;
-                }
-                case 'c': {
-                    putchar(((char)*ap++));
+                case 'e':
+                    eprint(va_arg(ap, float), precision);
                     break;
-                }
-                default: putchar(*c);
+                case 's':
+                    printf(va_arg(ap, char*));
+                    break;
+                case 'c':
+                    putchar(va_arg(ap, char));
+                    break;
+                case 'o':
+                    oprint(va_arg(ap, unsigned));
+                    break;
+                case 'n':
+                    *va_arg(ap, unsigned*) = n;
+                    break;
+                default:
+                    putchar(*c);
             }
         } else {
             putchar(*c);
         }
     }
-    (ap = (int*)0);
+    (ap = (void*)0);
 }
+#undef va_arg
