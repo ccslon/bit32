fde8d4df3284d035a71c98297b5b279bb4968ed0
ccslon <143284758+ccslon@users.noreply.github.com>
2026-06-30 10:10:21 -0400

    Update fact.c

--- cprograms/fact.c
+++ cprograms/fact.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 unsigned int fact(unsigned int n) {
-    if (n == 0) {
+    if (n == 0)
         return 1;
-    }
     return n * fact(n-1);
+}
+unsigned int fact(unsigned int n) {
+    unsigned int f = 1;
+    while (n)
+        f *= n--;
+    return f;
 }
 int main() {
     printf("%d\n", fact(6));
